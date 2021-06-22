76d6addfb7be0579aa40f167387c7f9aea53b83f
Sam <samjeffcoat@gmail.com>
2021-06-22 16:59:22 -0500

    basic react search built

---
+++ react-github-users/src/App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+      Search a GitHub User
+    </h1>
+    <Form/>
+  </div>
+  );
+}
+
+export default App;
---
+++ react-github-users/src/components/Form.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react"
+
+
+
+const Form = (props) => {
+    const [username, setUsername] = useState("")
+    const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(username)
+}
+    return( 
+    <form className = "w-full max-w-sm mx-auto" onSubmit = {handleSubmit}>
+        <div className = "mt-10 flex">
+            <input 
+            type="text" 
+            placeholder = "Github username"
+            className="form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150" 
+            value = {username}
+            onChange = {(event) => setUsername(event.target.value)}
+            required
+            />
+            <button
+                type = "submit"
+                className = "ml-2 py-2 px-3 border border-gray-300 rounded-md text-sm leading-4 font-medium text-gray-700 hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+            >
+                Search
+            </button>
+        </div>
+    </form>)
+}
+
+export default Form
